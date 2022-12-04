0f5892c45e4d6a5bea296d7982faa2ad37811325
Bernardo Moreira <moreira.bernardo@yahoo.com.br>
2022-12-04 20:02:23 -0300

    Ajuste em prod

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,35 +90,72 @@
 
     <script>
         $(document).ready(function() {
-            $.get("{{ asset('res/components/chart.html') }}", function(data) {
-                $('#main').html(data)
-                chartInit()
-            });
+            $.get('api/companies', function(data) {
+                $('#loader').addClass('hide-loader');
 
-            $('.nav-link').on('click', function() {
-                $('.nav-link').removeClass('active')
-                $(this).addClass('active')
+                companyList = data
 
-                $('#main').empty()
+                data.forEach(element => {
+                    $('#dropdown-company').append(
+                        '<a class="dropdown-item company-option" href="" data-company-id="' +
+                        element.id +
+                        '" data-company-name="' +
+                        element.company_name +
+                        '" data-company-doc="' +
+                        element.company_document +
+                        '">' +
+                        element.company_name +
+                        '</a>'
+                    )
 
-                destroyChart()
+                    $('#dropdown-company').append('<div class="dropdown-divider"></div>')
+                });
 
-                switch ($(this).attr('id')) {
-                    case 'dispenser':
-                        $.get("{{ asset('res/components/dispenser.html') }}", function(data) {
-                            $('#main').html(data)
-                            chartInit()
-                        });
-                        break;
-                    case 'map':
-                        break;
-                    case 'chart':
-                        $.get("{{ asset('res/components/chart.html') }}", function(data) {
-                            $('#main').html(data)
-                            chartInit()
-                        });
-                        break;
-                }
+                // seta o evento de trocar de empresa
+                $('#dropdown-company a').on('click', function() {
+                    $('#dropdown-company a').removeClass('active')
+                    $(this).addClass('active')
+
+                    $('#companyLabel').empty()
+                    $('#companyLabel').append('<span>' + $(this).data('companyName') + '</span>')
+                    $('#companyLabel').attr('data-company-id', $(this).data('companyId'))
+
+                    $('#dropdown-company').removeClass('show')
+                })
+
+                $.get("{{ asset('res/components/chart.html') }}", function(data) {
+                    $('#main').html(data)
+                    chartInit()
+                });
+
+                $('.nav-link').on('click', function() {
+                    $('.nav-link').removeClass('active')
+                    $(this).addClass('active')
+
+                    $('#main').empty()
+
+                    destroyChart()
+
+                    switch ($(this).attr('id')) {
+                        case 'dispenser':
+                            $.get("{{ asset('res/components/dispenser.html') }}", function(data) {
+                                $('#main').html(data)
+                                chartInit()
+                            });
+                            break;
+                        case 'map':
+                            break;
+                        case 'chart':
+                            $.get("{{ asset('res/components/chart.html') }}", function(data) {
+                                $('#main').html(data)
+                                chartInit()
+                            });
+                            break;
+                    }
+                })
+
+            }).fail(function(e) {
+                console.log(e)
             })
         })
     </script>
